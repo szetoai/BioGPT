2c032584df41e6f14ee2a88c65ffa4fe58b738b3
ASzeto99910 <83287704+AidanS99910@users.noreply.github.com>
2025-01-09 23:27:28 -0500

    me when i leak my api key

--- bio-gpt/src/components/LLM.tsx
+++ bio-gpt/src/components/LLM.tsx
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey:
-    "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",
+    "",
   dangerouslyAllowBrowser: true,
 });
 
